5f8e6f2d6a1b6c375c32f19eaa231145bdd1672b
Siarhei Stasiulevich <deform@tut.by>
2021-10-02 19:15:13 +0300

    Add method getItem
    In class "GeoUtil" added method "getItem" for
    to retrieve the object Item from the Json response.

--- src/main/java/com/senla/Geolocationconversionsystem/util/GeoUtil.java
+++ src/main/java/com/senla/Geolocationconversionsystem/util/GeoUtil.java
@@ -1,7 +1,30 @@
 package com.senla.Geolocationconversionsystem.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.senla.Geolocationconversionsystem.entity.Item;
+import com.senla.Geolocationconversionsystem.entity.ResultObject;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.ResponseBody;
 
 public class GeoUtil {
+
+    public static Item getItem(Request request) {
+
+        ResultObject resultObject = null;
+
+        OkHttpClient client = new OkHttpClient();
+        try {
+            ResponseBody responseBody = client.newCall(request).execute().body();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            resultObject = objectMapper.readValue(responseBody.string(), ResultObject.class);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return resultObject.getItems().get(0);
+    }
 
 }
