3dac7697c58dacdd00742eb2f6a3ffd33cf30944
Siarhei Stasiulevich <deform@tut.by>
2021-10-02 19:34:24 +0300

    Add method getPosition
    In class "GeoService" added method "getPosition".
    This method returns longitude and latitude for address.

--- src/main/java/com/senla/Geolocationconversionsystem/service/GeoService.java
+++ src/main/java/com/senla/Geolocationconversionsystem/service/GeoService.java
@@ -1,4 +1,23 @@
 package com.senla.Geolocationconversionsystem.service;
 
+import com.senla.Geolocationconversionsystem.util.GeoUtil;
+import com.squareup.okhttp.Request;
+
 public class GeoService {
+
+  private final String POSITION_PREF_URL = "https://geocode.search.hereapi.com/v1/geocode?q=";
+  private final String ADDRESS_PREF_URL = "https://revgeocode.search.hereapi.com/v1/revgeocode?at=";
+  private final String PART_URL_FOR_KEY = "&apiKey=";
+  private final String KEY_API = "aMMQjymQ3US33cQeVSsix0hmHYUFT59z0yXRZWcpD2w";
+
+  public String getPosition(String address) {
+
+    Request request =
+        new Request.Builder()
+            .url(POSITION_PREF_URL + address + PART_URL_FOR_KEY + KEY_API)
+            .get()
+            .build();
+
+    return GeoUtil.getItem(request).getPosition().toString();
+  }
 }
