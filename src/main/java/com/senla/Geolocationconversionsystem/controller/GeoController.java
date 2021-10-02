f009ac6a7427c49b1a7da3632945291020407299
Siarhei Stasiulevich <deform@tut.by>
2021-10-02 19:47:44 +0300

    Add method getPosition
    In class "GeoController" added method "getPosition".

--- src/main/java/com/senla/Geolocationconversionsystem/controller/GeoController.java
+++ src/main/java/com/senla/Geolocationconversionsystem/controller/GeoController.java
@@ -1,4 +1,23 @@
 package com.senla.Geolocationconversionsystem.controller;
 
+import com.senla.Geolocationconversionsystem.service.GeoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GeoController {
+
+  GeoService geoService;
+
+  @Autowired
+  public GeoController(GeoService geoService) {
+    this.geoService = geoService;
+  }
+
+  @GetMapping("/position/{address}")
+  public String getPosition(@PathVariable String address) {
+    return geoService.getPosition(address);
+  }
 }
