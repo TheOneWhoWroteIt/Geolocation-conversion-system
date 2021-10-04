58cd45f422af5143a4ae91b842d24d09fdd5e64e
Siarhei Stasiulevich <deform@tut.by>
2021-10-04 17:44:52 +0300

    Create class GeoControllerTest

---
+++ src/test/java/com/senla/Geolocationconversionsystem/controller/GeoControllerTest.java
@@ -0,0 +1,14 @@
+package com.senla.Geolocationconversionsystem.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeoControllerTest {
+
+  @Test
+  void getPosition() {}
+
+  @Test
+  void getAddress() {}
+}
