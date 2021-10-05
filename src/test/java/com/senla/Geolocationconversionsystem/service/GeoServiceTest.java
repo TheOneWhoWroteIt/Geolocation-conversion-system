29be6c6771c8ec72a5e8ae4d212196cbdb7c5ce2
Siarhei Stasiulevich <deform@tut.by>
2021-10-05 20:12:54 +0300

    Add methods for get data from cache

--- src/test/java/com/senla/Geolocationconversionsystem/service/GeoServiceTest.java
+++ src/test/java/com/senla/Geolocationconversionsystem/service/GeoServiceTest.java
@@ -7,6 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.CacheManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
+import static java.util.Optional.ofNullable;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class GeoServiceTest {
@@ -19,4 +23,14 @@ class GeoServiceTest {
     geoService.getPosition("гродно");
     geoService.getAddress("53.93133,27.64601");
   }
+
+  private Optional<String> getAddressFromCache(String position){
+      return ofNullable(cacheManager.getCache("address")).map(c -> c.get(position, String.class));
+  }
+
+  private Optional<String> getPositionFromCache(String address){
+      return ofNullable(cacheManager.getCache("position")).map(c -> c.get(address, String.class));
+  }
+
+
 }
