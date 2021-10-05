303413540a6e8ea46db5c345beb8f2de8e0441e6
Siarhei Stasiulevich <deform@tut.by>
2021-10-05 20:04:48 +0300

    Add method setDataToCache

--- src/test/java/com/senla/Geolocationconversionsystem/service/GeoServiceTest.java
+++ src/test/java/com/senla/Geolocationconversionsystem/service/GeoServiceTest.java
@@ -1,7 +1,22 @@
 package com.senla.Geolocationconversionsystem.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.CacheManager;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class GeoServiceTest {
 
+  @Autowired private GeoService geoService;
+  @Autowired private CacheManager cacheManager;
+
+  @BeforeEach
+  void setDataToCache() {
+    geoService.getPosition("гродно");
+    geoService.getAddress("53.93133,27.64601");
+  }
 }
